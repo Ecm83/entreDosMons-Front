395768c7e4a9a75da3fbcad74917c939860907de
Enric <enricldn@gamil.com>
2024-04-22 11:53:55 +0200

    feat: Suppliers page added and LeftMenu component in molecules remooved

--- src/lib/components/molecules/index.js
+++ src/lib/components/molecules/index.js
@@ -1,3 +1,2 @@
-export { default as LeftMenu } from './LeftMenu.svelte';
 export { default as DoubleSelect } from './DoubleSelect.svelte';
 export { default as VoiceRecognition } from './VoiceRecognition.svelte';
--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -21,12 +21,13 @@
 				<SidebarWrapper>
 					<SidebarGroup>
 						<SidebarItem label="Panell de Control" href="/dashboard"></SidebarItem>
+						<SidebarItem label="Proveïdors" href="/dashboard/suppliers"></SidebarItem>
 						<SidebarItem label="Paísos" href="/dashboard/countries"></SidebarItem>
 						<SidebarItem label="Regions" href="/dashboard/regions"></SidebarItem>
 						<SidebarItem label="Tipus de vi" href="/dashboard/wineTypes"></SidebarItem>
 						<SidebarItem label="Raïms" href="/dashboard/grapes"></SidebarItem>
 						<SidebarItem label="Cellers" href="/dashboard/cellars"></SidebarItem>
-						<SidebarItem label="Terres" href="/dashboard/soils"></SidebarItem>
+						<SidebarItem label="Sòls" href="/dashboard/soils"></SidebarItem>
 						<SidebarItem label="Sulfits" href="/dashboard/sulphites"></SidebarItem>
 						<SidebarItem label="Vins" href="/dashboard/wines"></SidebarItem>
 						<SidebarItem label="Stock" href="/dashboard/stock"></SidebarItem>
